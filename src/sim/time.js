ea4ea70b6eca219dcbaeb3ce69e18c50cfeef1ba
Mattias <mattiasnogols@protonmail.com>
2026-09-16 23:17:46 +0300

    add pause/speed

--- src/sim/time.js
+++ src/sim/time.js
@@ -2,13 +2,28 @@ import { DAYS_PER_SECOND } from '../config.js'
 
 export function createSimTime(daysPerSecond = DAYS_PER_SECOND) {
   let simDays = 0
+  let paused = false
+  let speed = 1
 
   return {
     advance(deltaSeconds) {
-      simDays += deltaSeconds * daysPerSecond
+      if (paused) return
+      simDays += deltaSeconds * daysPerSecond * speed
     },
     getDays() {
       return simDays
+    },
+    setPaused(value) {
+      paused = value
+    },
+    isPaused() {
+      return paused
+    },
+    setSpeed(value) {
+      speed = Math.max(value, 0)
+    },
+    getSpeed() {
+      return speed
     },
     reset() {
       simDays = 0
