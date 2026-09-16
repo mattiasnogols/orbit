cfefe1f8cf4243655b4dc5ba8ad2af73d573ca20
Mattias <mattiasnogols@protonmail.com>
2026-09-16 22:08:38 +0300

    wire lightning

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import * as THREE from 'three'
 import { createScene } from './scene/setup.js'
+import { addLighting } from './scene/lighting.js'
 
 const { renderer, scene, camera, controls } = createScene()
+addLighting(scene)
 
 scene.background = new THREE.Color(0x101820)
 
