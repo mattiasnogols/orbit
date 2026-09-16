488ed431e73bcc216e19b40128a86e9bcd6f7afc
Mattias <mattiasnogols@protonmail.com>
2026-09-16 22:11:24 +0300

    skeleton done

--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ addLighting(scene)
 
 scene.background = new THREE.Color(0x101820)
 
+const testSphere = new THREE.Mesh(
+  new THREE.SphereGeometry(1, 48, 32),
+  new THREE.MeshStandardMaterial({ color: 0xffcc33, roughness: 0.8 }),
+)
+testSphere.position.set(3, 0, 0)
+scene.add(testSphere)
+
 renderer.setAnimationLoop(() => {
   controls.update()
   renderer.render(scene, camera)
